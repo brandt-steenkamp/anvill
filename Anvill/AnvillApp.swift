3cc91cc94168a2b1722a457de1c61339eb9138ec
Brandt Steenkamp <brandt.steenkamp@gmail.com>
2026-01-31 16:19:03 +0200

    Initial Commit

---
+++ Anvill/AnvillApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnvillApp.swift
+//  Anvill
+//
+//  Created by Brandt steenkamp on 2026/01/31.
+//
+
+import SwiftUI
+
+@main
+struct AnvillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
